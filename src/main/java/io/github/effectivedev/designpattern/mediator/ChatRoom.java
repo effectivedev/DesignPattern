82ed255b7b7b373976ceeebad14b4b713d5b4bbe
Inyoung <in0@me.com>
2019-07-17 13:24:05 +0900

    add mediator class

---
+++ src/main/java/io/github/effectivedev/designpattern/mediator/ChatRoom.java
@@ -0,0 +1,9 @@
+package io.github.effectivedev.designpattern.mediator;
+
+import java.util.Date;
+
+public class ChatRoom {
+    public static void showMessage(User user, String message){
+        System.out.println(new Date().toString()+ " ["+ user.getName() + "]");
+    }
+}
